850822073b4620d30720c9eec63d564757fbd748
Pratik Anand <pratik@Pratiks-MacBook-Air.local>
2025-06-26 19:47:29 +0530

    Today's Assignment done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export function App() {
+  const [data, setData] = useState("");
+  
+  const getValue=(event) => {
+    console.log(event.target.value);
+    setData(data.concat(event.target.value));
+  }
+  const clear = () => {
+    setData("");
+  }
+
+  const calculate = () => {
+    setData(eval(data).toString());
+  }
   return (
     <>
-  <h1>Hello</h1>
+      <div>
+        <h1>Calculator</h1>
+      </div>
+      <div className='conatiner'>
+        <div>
+          <input placeholder="0" value={data} />
+        </div>
+      </div>
+
+      <div className="buttons" >
+        <button onClick={getValue} value="1">1</button>
+        <button onClick={getValue} value="2">2</button>
+        <button onClick={getValue} value="3">3</button>
+        <button onClick={getValue} value="4">4</button>
+        <button onClick={getValue} value="5">5</button>
+        <button onClick={getValue} value="6">6</button>
+        <button onClick={getValue} value="7">7</button>
+        <button onClick={getValue} value="8">8</button>
+        <button onClick={getValue} value="9">9</button>
+        <button onClick={getValue} value="0">0</button>
+        <button onClick={getValue} value=".">.</button>
+        <button onClick={getValue} value="/">/</button>
+        <button onClick={getValue} value="*">*</button>
+        <button onClick={getValue} value="+">+</button>
+        <button onClick={getValue} value="-">-</button>
+        <button onClick={calculate}>=</button>
+        <button onClick={clear}>AC</button>
+      </div>
     </>
   )
 }
-
-export default App
-
-
--- src/App2.jsx
+++ src/App2.jsx
@@ -1,15 +1,8 @@
-// Function Component using JSX
-function Welcome(props) {
-  return <h1>Welcome, {props.name}</h1>;
-}
-
 // Using the component
 export function App2() {
   return (
     <div>
-        <h1>hi</h1>
-      <Welcome name="Alice" />
-      <Welcome name="Bob" />
+      <h1>hello</h1>
     </div>
   );
 }
